8b05a6849093103088f623b92ebba9583c2a1bd2
athiely <138408141+athiely@users.noreply.github.com>
2025-05-28 14:26:15 -0300

    Add files via upload

--- Tarefa01_athiely/Tarefa01_athiely/src/componentes/Card.jsx
+++ Tarefa01_athiely/Tarefa01_athiely/src/componentes/Card.jsx
@@ -1,11 +1,19 @@
 import './Card.css'
 import garfield_img from "../assets/personagem/personagem.jpg"
 import cinderella_img from "../assets/personagem/GarfieldCharacter.webp"
+import pica_pau_img from "../assets/personagem/Pica_pau.webp"
+import tecna_img from "../assets/personagem/tecna.png"
+import barbie_img from "../assets/personagem/barbie.png"
+import flora_img from "../assets/personagem/flora.png"
 
 
 const personagens = [
     {name: "Cinderella", image: garfield_img, caption: "Princesa da Disney"},
-    {name: "Garfield", image: cinderella_img, caption: "Gato que fala"}
+    {name: "Garfield", image: cinderella_img, caption: "Gato que fala"},
+    {name: "Pica-Pau", image: pica_pau_img, caption: "Passarinho"},
+    {name: "Tecna", image: tecna_img, caption: "Fada da Tecnologia"},
+    {name: "Barbie", image: barbie_img, caption: "Barbie MODA E MAGIA"},
+    {name: "Flora", image: flora_img, caption: "Fada da Natureza"}
 ]
 export default function Card() {
     return (
