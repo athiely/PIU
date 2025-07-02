520b7b08694f4ad30dd2d8208231d7cfe8cbd3fe
Athiely Taiany Fernandes Araújo <20221101110035@ifrn.local>
2025-07-02 14:17:51 -0300

    commit

---
+++ lista_tarefas/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import FirstForm from './componentes/FirstForm'
+import FormState from './componentes/FormState'
+import FormSelect from './componentes/FormSelect'
+import Listar from './componentes/Listar'
+
+function App() {
+ 
+
+  return (
+  <div className="App">
+    {/* <h1>Trabalhando com Formulários</h1>
+      <FirstForm/>
+      <br />
+      <FormState/>
+      <br />
+      <FormSelect/> */}
+      <Listar/>
+
+  </div>
+  )
+}
+
+export default App
